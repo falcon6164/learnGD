6c24d1293904fe6050809c4dc6b0d3461f67a683
Gray Home PC <falcon6164@gmail.com>
2026-06-05 02:39:57 +0800

    working on level up and UIs

---
+++ UpgradeDB.gd
@@ -0,0 +1,34 @@
+extends Node
+
+# const ICON_PATH = #技能的ICON圖示位置
+
+const UPGRADES = {
+	"MultiShot1": {
+		"icon": "技能圖示", 
+		"displayname": "Multishot", 
+		"description": "shoots additional arrow with lesser power.", 
+		"Level": "Lv1", 
+		"prerequisite": []
+	},
+		"StrongBow1": {
+		"icon": "技能圖示", 
+		"displayname": "Strong Bow", 
+		"description": "Extends the power and speed while charging up a bowshot.", 
+		"Level": "Lv1", 
+		"prerequisite": []
+	},
+		"Piercing1": {
+		"icon": "技能圖示", 
+		"displayname": "Piercing", 
+		"description": "Arrow will go through enemy.", 
+		"Level": "Lv1", 
+		"prerequisite": []
+	},
+		"Accuracy1": {
+		"icon": "技能圖示", 
+		"displayname": "Accuracy", 
+		"description": "The more you practice, the better the aim.", 
+		"Level": "Lv1", 
+		"prerequisite": []
+	},
+}
